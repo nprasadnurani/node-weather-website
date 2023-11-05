c2e78bb424365a93761f25ac66744a75cce28e89
Prasad Nurani <prasadneelakandan@gmail.com>
2023-11-05 06:44:43 -0600

    removed unwanted prints from app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,3 @@
-console.log('Client side javascript file is loaded')
-
-//fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//    response.json().then((data) => {
-//        console.log(data)
-//    })
-
-//})
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const message1 = document.querySelector('#message-1')
