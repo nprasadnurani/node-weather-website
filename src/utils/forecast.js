f8f24ff9405fea700b6a21c55e3ad7467ff83766
Prasad Nurani <prasadneelakandan@gmail.com>
2023-11-05 09:48:27 -0600

    change the text feels like

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,7 @@ const forecast = (long,lat,callback) => {
               callback('Location not found, try another one', undefined)
     
           } else {
-                callback(undefined, body.current.weather_descriptions[0] + '. Current temperature is:' + body.current.temperature + ' but feels like: ' + body.current.feelslike + '. The humidity for today is ' + body.current.humidity )
+                callback(undefined, body.current.weather_descriptions[0] + '. Current temperature is:' + body.current.temperature + ' and feels like: ' + body.current.feelslike + '. The humidity is ' + body.current.humidity + '%' )
           }
     })
     }
